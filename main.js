d0135b1a3f6bf74ca88679c101c0e4b4c4b980d4
isabella <105920830+isa1323@users.noreply.github.com>
2023-11-09 08:26:45 -0300

    m

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-function tocasom(idElementoAudio) {
+function tocaSom(idElementoAudio){
     document.querySelectorAll(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador<listaDeTeclas.length){
+while(contador < listaDeTeclas.length){
     const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_"+efeito;
     listaDeTeclas[contador].onclick = function(){
         tocaSom(idAudio)
-    
     }
     contador = contador + 1;
     console.log(contador);
